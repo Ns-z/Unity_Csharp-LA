143f541fe37d32d2e280e0a5c78f4111c4f98251
Ns-z <nsgazioglu@gmail.com>
2025-09-09 13:33:28 +0300

    Enums-0.01
    Enum learning-1

--- Assets/Scripts/LearnEnums_1.cs
+++ Assets/Scripts/LearnEnums_1.cs
@@ -1,6 +1,37 @@
 using UnityEngine;
+using System;
 
-public class LearnEnums_1 : MonoBehaviour
-{
-    
+public class LearnEnums_1 : MonoBehaviour {
+
+    public void Start() {
+        Debug.Log("Starting Enums --> ");
+
+
+        // Enum.Parse -> enum turune erisebilmek icin using System; kutuphanesi implemente edildi.
+
+        // Enum degerleri stringe cevrildi.
+        PlayerActions playerActions1 = PlayerActions.PlayerStart;
+        string playerActionString = playerActions1.ToString();
+        Debug.Log(playerActionString);
+        
+        // Burada stringe cevrilmis enum degerlerini tekrar Enuma cevirip yazdiriyoruz
+        Debug.Log("ikinci part");
+        Debug.Log(Enum.Parse<PlayerActions>(playerActionString));
+        
+        //  foreach dongusu ile bu sekilde Enum degerlerini dondurebiliriz/
+        Debug.Log("third part \\\\");
+        foreach (PlayerActions playerActions in Enum.GetValues(typeof(PlayerActions))) {
+            Debug.Log(playerActions);
+
+        }
+    }
+
+    public enum PlayerActions {
+        PlayerStart,
+        PlayerPause,
+        PlayerResume
+    }
+
+
+
 }
