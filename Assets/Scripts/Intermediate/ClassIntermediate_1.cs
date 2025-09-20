9a40785368e0c3fb891ebd86ac95041a59104c43
Ns-z <nsgazioglu@gmail.com>
2025-09-20 23:48:21 +0300

    Class_Intermediate1
    intermediate level class learning 1

--- Assets/Scripts/Intermediate/ClassIntermediate_1.cs
+++ Assets/Scripts/Intermediate/ClassIntermediate_1.cs
@@ -4,8 +4,59 @@ public class ClassIntermediate_1 : MonoBehaviour
 {
     void Start()
     {
-        
+
+        Unit unit = new Unit();
+        Character character = new Character("Enes");
+        Enemy enemy = new Enemy();
+
+        unit.Move();
+        character.Move();
+        enemy.Move();
+
+        // ToString function automatically implemented on every class objects.
+        // so you can override ToString function
+        Debug.Log(character.ToString());
+        Debug.Log(character); // ToString Function automatically called.
+         
     }
 
+    public class Unit {
+
+        public int speed;
+        
+        public virtual void Move() {
+            Debug.Log("Move");
+        }
+    }
     
+    public class Enemy : Unit {
+
+        public override void Move() {
+            Debug.Log("Move Enemy");
+        }
+    }
+
+    // has a override function Inheritance
+    public class Character : Unit {
+
+        public string playerName;
+
+        public Character(string playerName) {
+            speed = 5;
+            this.playerName = playerName;
+        }
+
+
+        public override void Move() {
+            Debug.Log("Move Character");
+        }
+
+        // Trying Override ToString Function
+        public override string ToString() {
+            return "Player: " + playerName;
+        }
+    }
+
 }
+
+
